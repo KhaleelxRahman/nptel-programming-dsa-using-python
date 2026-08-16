2c597b2b1af90f38dcd7b92d5c18259a308f1cb3
MD.KHALEEL-UR-RAHMAN <mdkhaleelurrahman51@gmail.com>
2026-08-16 13:33:03 +0530

    Lecture 03 : DATA TYPES IN PYTHON

---
+++ Week-01/data types in python.py
@@ -0,0 +1,31 @@
+x = int(10)
+print(type(x))
+
+y = float(12.7)
+print(type(y))
+
+z = 3 + 4j
+print(type(z))
+
+p = True
+print(type(p))
+
+q = "hello"
+print(type(q))
+
+r = [1, 2, 3]
+print(type(r))
+
+s = (1, 2, 3)
+print(type(s))
+
+t = {1, 2, 3}
+print(type(t))
+
+u = {"name": "khaleel"}
+print(type(u))
+
+v = None
+print(type(None))
+
+
