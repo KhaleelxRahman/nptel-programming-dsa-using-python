396023470121c51c72f5eb48034716181e25422f
MD.KHALEEL-UR-RAHMAN <mdkhaleelurrahman51@gmail.com>
2026-08-16 22:11:40 +0530

    Add Week 2 Lecture 2 string operations

---
+++ Week-02/operations on string.py
@@ -0,0 +1,59 @@
+#CONCATENATION
+x = "hello"
+y = "world"
+z = x +" " + y 
+# THE  QUOTES ARE THE SPACE PROIDES SPACE BETWEEN THE TWO WORDS 
+print(z)
+#REPETITION
+A = "PYTHON "
+
+print(A*3)
+#INDEXING
+B = "PYTHON"
+
+print(B[0])
+print(B[1])
+print(B[3])
+print(B[5])
+#NEGATIVE INDEXING
+print(B[-1])
+print(B[-3])
+print(B[-5])
+#STRING SLICING
+C = "PYTHON"
+
+print(C[0:2])
+print(C[0:])
+print(C[:5])
+#NEGATIVE SLICING
+print(C[0:-1])
+print(C[-4:])
+print(C[-3:])
+#REVERSE STRING
+D = "PYTHON"
+
+print(D[::-1])
+#IN MEMBERSHIP
+E = "Python"
+
+print("P" in E)
+print("th" in E)
+print("z" in E)
+#NOT IN MEMBERSHIP
+F = "Python"
+
+print("z" not in F)
+print("P" not in F)
+#COMPARISON OPERATIONS IN STRING
+G = "apple"
+H = "banana"
+
+print(G == H)
+print(G != H)
+print(G <  H) # a COMES BEFORE b ALPHABETICALLY
+print(G >  H)
+print(G <= H)
+print(G >= H)
+
+
+
