b7a1ae213e7f903dcbd1f4cf8a1181a09cc362b0
MD.KHALEEL-UR-RAHMAN <mdkhaleelurrahman51@gmail.com>
2026-08-16 12:46:04 +0530

    LECTURE 03: Using While loop in Gcd

---
+++ Week-01/using while loop in gcd.py
@@ -0,0 +1,15 @@
+def gcd(m,n):
+
+    i = min(m,n)
+
+    while i>0:
+
+        if(m%i)==0 and (n%i)==0:
+            return i
+        
+        else :
+         i = i-1
+
+m = int(input("enter m:"))
+n = int(input("enter n:"))
+print("GCD:",gcd(m,n))
